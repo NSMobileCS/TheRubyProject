11fd1547eee15b0e3c4826f01051c2e1ef460f90
Nathan R Smith <nrsmith012@gmail.com>
2018-01-02 19:37:32 -0800

    commit 1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, presence: true, length: { in: 2..40 }
+  validates :email, presence: true, length: { in: 2..40 }, format: { with: EMAIL_REGEX }
+
+
+  before_save :email_lowercase
+
+  def email_lowercase
+    email.downcase!
+  end
+
 end
