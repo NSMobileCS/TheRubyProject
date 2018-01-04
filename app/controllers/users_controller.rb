60fc0cf33654c4e7bc3cd13c94647038574dea58
Nathan R Smith <nrsmith012@gmail.com>
2018-01-03 18:23:28 -0800

    pre preparation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def new
+    u = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation] )
+    if u.save
+      session[:user_id] = u.id
+      redirect_to "/secrets"
+    else
+      flash[:errors] = ["there was a problem with your username or password"]
+      session[:user_id] = nil
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def show
+    if session[:user_id]
+      if session[:user_id] == params[:id]
+        @user = User.find(params[:id])
+      else
+        flash[:errors] = ["you may only view your own user page"]
+        redirect_to '/users/'+User.find(session[:user_id]).id.to_s+'/show'
+    else
+      flash[:errors] = ["you must be logged in to view user pages"]
+      session[:user_id] = nil
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def listshow
+    @users = User.all
+  end
+
+  def edit
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  post 'users/new' => 'users#new'
+
+  get 'users/:id/show' => 'users#show'
+  get 'users/listshow'
+
+  get 'users/edit'
+
+  get 'sessions/new'
+  get '' => 'sessions#new'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
